095604ce328aecee6b2d4ccb4af7d6073489cacf
novakim77-glitch <novakim77@gmail.com>
2026-01-30 17:15:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,4 +10,10 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(clients.openWindow('/'));
+
+});
+// sw.js 파일 하단에 추가
+self.addEventListener('fetch', (event) => {
+  // 앱 설치 조건을 충족하기 위한 빈 핸들러
+  // 나중에는 오프라인 캐싱 기능을 여기에 넣을 수 있습니다.
 });
